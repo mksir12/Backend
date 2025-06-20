ed7547e2d9cf948593cecfe43bdd3297fb66d4ba
mksir12 <116490353+mksir12@users.noreply.github.com>
2025-06-20 23:04:08 +0530

    Update download.py

--- api/download.py
+++ api/download.py
@@ -12,7 +12,7 @@ HEADERS = {
                   "AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/91.0.4472.124 Safari/537.36",
     "Accept": "*/*",
-    "Cookie": "ndus=YzrYlCHteHuixx7IN5r0fc3sajSOYAHfqDoPM0dP"
+    "Cookie": "ndus=Y2f2tB1peHuigEgX5NpHQFeiY88k9XMojvuvxNVb"
 }
 
 DL_HEADERS = HEADERS.copy()
