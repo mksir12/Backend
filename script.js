5e986fb451312be6033a2207daeb3de2195821fa
mksir12 <116490353+mksir12@users.noreply.github.com>
2025-06-29 14:35:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function renderPreview() {
-  const html = document.getElementById("htmlInput").value;
-  const encoded = encodeURIComponent(btoa(html)); // base64 + URI encoding
-  window.location.href = `/viewer.html?code=${encoded}`;
-}
